3fc128bae596a2dc73e69716fd4d93ff245e81ad
Jason Wang <wangpiju0420@gmail.com>
2022-08-05 06:40:30 +0800

    Add done function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./styles.css";
 
 document.querySelector("#create").onclick = function () {
-  if (document.querySelector("#to-do-input").value.length == 0) {
+  if (document.querySelector("#to-do-input").value.length === 0) {
     alert("Please Type A Task In Your Mind.");
   } else {
     document.querySelector("#tasks").innerHTML += `
@@ -14,16 +14,24 @@ document.querySelector("#create").onclick = function () {
                 <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
               </svg>
              </button>
+             </button>
+              <button  type="button" class="done btn btn-success">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16">
+                  <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
+                </svg>
+              </button>
               <button  type="button" class="delete btn btn-danger">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
                   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
                 </svg>
               </button>
+
           </div>
       `;
 
     var current_tasks = document.querySelectorAll(".delete");
     var edit_tasks = document.querySelectorAll(".edit");
+    var done_tasks = document.querySelectorAll(".done");
 
     for (var i = 0; i < current_tasks.length; i++) {
       current_tasks[i].onclick = function () {
@@ -34,6 +42,9 @@ document.querySelector("#create").onclick = function () {
         this.previousElementSibling.textContent = document.querySelector(
           "#to-do-input"
         ).value;
+      };
+      done_tasks[i].onclick = function () {
+        this.parentNode.style.backgroundColor = "green";
       };
     }
   }
